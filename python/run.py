00218d7a79a15a5c63250ade1c3e08668a5e168c
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2025-01-27 10:00:27 +0100

    fix: Create run.py

---
+++ python/run.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+
+def insecure_function():
+    # Hardcoded secret (B105:hardcoded_password_string)
+    password = "supersecretpassword"
+
+    # Unsafe use of subprocess (B602:subprocess_popen_with_shell_equals_true)
+    command = "ls -l"
+    subprocess.Popen(command, shell=True)
+
+    # Insecure use of eval (B307:eval)
+    user_input = input("Enter some code to execute: ")
+    eval(user_input)
+
+    # Weak cryptographic key (B303:md5)
+    import hashlib
+    m = hashlib.md5()
+    m.update(b"Hello, world!")
+    print(m.hexdigest())
+
+if __name__ == "__main__":
+    insecure_function()
